d51e720a66804b0770694947a526673bd2276abc
Jake Lazarus <jake@replicahq.com>
2021-11-04 15:20:29 -0500

    move

--- sqlalchemy_bigquery/base.py
+++ sqlalchemy_bigquery/base.py
@@ -191,6 +191,17 @@ class BigQueryCompiler(_struct.SQLCompiler, SQLCompiler):
             kwargs["compile_kwargs"] = util.immutabledict({"include_table": False})
         super(BigQueryCompiler, self).__init__(dialect, statement, *args, **kwargs)
 
+    def visit_array(self, element, **kw):
+        print("ARRAY HURRAH")
+        return "[%s]" % self.visit_clauselist(element, **kw)
+
+    def visit_struct(self, element, within_columns_clause=True, **kw):
+        if element.field:
+            return self.preparer.quote_column(element.field)
+        kw["within_columns_clause"] = True
+        values = self.visit_clauselist(element, **kw)
+        return "struct(%s)" % values
+
     def visit_insert(self, insert_stmt, asfrom=False, **kw):
         # The (internal) documentation for `inline` is confusing, but
         # having `inline` be true prevents us from generating default
@@ -568,19 +579,7 @@ class BigQueryTypeCompiler(GenericTypeCompiler):
     visit_VARCHAR = visit_NVARCHAR = visit_TEXT = visit_STRING
 
     def visit_ARRAY(self, type_, **kw):
-        print("HOORAY BOOO")
         return "ARRAY<{}>".format(self.process(type_.item_type, **kw))
-
-    def visit_array(self, element, **kw):
-        print("ARRAY HURRAH")
-        return "[%s]" % self.visit_clauselist(element, **kw)
-
-    def visit_struct(self, element, within_columns_clause=True, **kw):
-        if element.field:
-            return self.preparer.quote_column(element.field)
-        kw["within_columns_clause"] = True
-        values = self.visit_clauselist(element, **kw)
-        return "struct(%s)" % values
 
     def visit_BINARY(self, type_, **kw):
         if type_.length is not None:
